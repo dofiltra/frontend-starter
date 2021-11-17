7a020552201b2955bea165e15251ae57b2fc44b3
Dmitriy <pointsman1@gmail.com>
2021-11-17 17:38:54 +0200

    env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,16 +1,17 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASURMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY?.toString(),
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN?.toString(),
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL?.toString(),
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID?.toString(),
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET?.toString(),
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID?.toString(),
+  appId: import.meta.env.VITE_FIREBASE_APP_ID?.toString(),
+  measurementId: import.meta.env.VITE_FIREBASE_MEASURMENT_ID?.toString(),
 }
 
 const app = initializeApp(firebaseConfig)

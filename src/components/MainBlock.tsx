a67cf5ae1c7683b8e21200423d6de383b3427b4e
Dmitriy <pointsman1@gmail.com>
2021-11-17 19:11:38 +0200

    .

--- src/components/MainBlock.tsx
+++ src/components/MainBlock.tsx
@@ -1,14 +1,10 @@
 import { BodyText } from 'components/Text'
-import { useContext } from 'preact/hooks'
 import { useLocalize } from '@borodutch-labs/localize-react'
-import AuthContext from './Auth/AuthContext'
 import useUserCount from 'hooks/useUserCount'
 
 export default function MainBlock() {
   const { userCount } = useUserCount()
   const { translate } = useLocalize()
-  const { user } = useContext(AuthContext)
-  console.log(user)
 
   return (
     <>
